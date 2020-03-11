d5c480b7128ce1a64b500f2834ee5928455c0015
Jason Bryan <jbryan@elastic.co>
2020-03-11 16:06:00 -0400

    add elasticsearch

--- useful_shell_commands.sh
+++ useful_shell_commands.sh
@@ -1,3 +1,7 @@
 # Openssl
 ## Openssl fetch certificate chain
 openssl s_client -host $SSLHOST -port $SSLPORT -showcerts < /dev/null > chain.pem
+
+# Elasticsearch
+## jvm.options - Adjust Java Heap
+sudo sed -i.bak 's/-Xmx2g/-Xmx256m/; s/-Xms2g/-Xms256m/' /etc/elasticsearch/jvm.options
